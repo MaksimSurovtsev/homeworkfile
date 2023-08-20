b12214c02ef337f285fcdd3615729121a5f77ac7
Maksim Surovtsev <maksimsurotsev90@mail.ru>
2023-08-20 16:41:11 +0300

    homewokfile

---
+++ main.py
@@ -0,0 +1,39 @@
+def read_recept(file_name):
+    cook_book = {}
+    with open(file_name, 'r', encoding='utf-8') as file:
+        while True:
+            dish_name = file.readline().strip()
+            if not dish_name:
+                break
+            ingredients_count = int(file.readline().strip())
+            ingredients = []
+            for _ in range(ingredients_count):
+                ingredient_info = file.readline().strip().split(' | ')
+                ingredient = {
+                    'ingredient_name': ingredient_info[0],
+                    'quantity': int(ingredient_info[1]),
+                    'measure': ingredient_info[2]
+                }
+                ingredients.append(ingredient)
+            cook_book[dish_name] = ingredients
+            file.readline()  # skip empty line between recipes
+    return cook_book
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        if dish in cook_book:
+            ingredients = cook_book[dish]
+            for ingredient in ingredients:
+                ingredient_name = ingredient['ingredient_name']
+                quantity = ingredient['quantity'] * person_count
+                measure = ingredient['measure']
+                if ingredient_name in shop_list:
+                    shop_list[ingredient_name]['quantity'] += quantity
+                else:
+                    shop_list[ingredient_name] = {'quantity': quantity, 'measure': measure}
+    return shop_list
+
+cook_book = read_recept('recept.txt')
+print(cook_book)
+shop_list = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+print(shop_list)
